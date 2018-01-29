e13fcc0c9e2aa540dca76719415553173ccfda9f
An Nuyen <annuyen@gmail.com>
2018-01-29 15:21:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function fuzzyMatch(drivers, string) {
   return drivers.filter(function(driver) {return driver.slice(0, string.length) === string})
 }
 
-function matchName(drivers, string) { }
+function matchName(drivers, string) {
+  return drivers.filter(function(driver) {return driver.name === string})
+}
